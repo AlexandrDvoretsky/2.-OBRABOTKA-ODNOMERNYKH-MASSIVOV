25ef0a3fa65d1238d1097794560a9ac2f0bffab1
AlexandrDvoretsky <36534001+AlexandrDvoretsky@users.noreply.github.com>
2018-07-10 15:24:04 +0300

    2.3 В массиве А(N) первый положительный элемент и последний отрицательный элемент переставить местами

--- 2.3.php
+++ 2.3.php
@@ -2,10 +2,19 @@
   // 2.3 В массиве А(N) первый положительный элемент и последний от-
   // рицательный элемент переставить местами
 
-  $mas=[1,2,-3,4,-5,6,7,-8,9];
-  $positionPol=9;
-  echo "Исходный массив : ";
-  for($i=0;$i<9;$i++)
+  $mas=[-3,-10,1,2,-3,4,-5,6,-7,-8,9,-30,99,1,-1];
+  //$positionPol=9;
+
+  echo "Исходный массив: ";
+  for($i=0;$i<999;$i++)
+  {
+    echo "$mas[$i] | ";
+    $count++;
+    if($mas[$i+1]=="") break;
+  }
+  $positionPol = $count;
+
+  for($i=0;$i<$count;$i++)
   {
     if($mas[$i]>0)
     {
@@ -20,12 +29,12 @@
       $elementOtr = $mas[$i];
       $positionOtr = $i;
     }
-    echo "$mas[$i] | ";
+
   }
   $mas[$positionPol] = $elementOtr;
   $mas[$positionOtr] = $elementPol;
   echo "<br>Измененный массив : ";
-  for($j=0;$j<9;$j++)
+  for($j=0;$j<$count;$j++)
   {
     echo "$mas[$j] | ";
   }
