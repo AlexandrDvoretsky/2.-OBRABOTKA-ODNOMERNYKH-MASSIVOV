91f62eda258166854829796a238a5b247d38bc7d
AlexandrDvoretsky <36534001+AlexandrDvoretsky@users.noreply.github.com>
2018-07-10 16:42:28 +0300

    2.10 В массиве А(N) все четные элементы заменить максимальным элементом, а нечетные - минимальным.

--- 2.10.php
+++ 2.10.php
@@ -1,26 +1,31 @@
 <?php
   // 2.10 В массиве А(N) все четные элементы заменить максимальным элементом, а нечетные - минимальным.
 
-  $mas=[1,2,3,4,5,6,7,8,9,10];
+  $mas=[0,1,2,3,4,5,6,7,8,9,10,11,12,-9,13,55];
+
   echo "Исходный массив: ";
-  $min=10;$max=0;
-  for($i=0;$i<10;$i++)
+  for($i=0;$i<999;$i++)
   {
     echo "$mas[$i] | ";
-    if($i%2==0)
+    $count++;
+    if($mas[$i]>$num)
     {
-      if($mas[$i]>$max) $max = $mas[$i];
-      $mas[$i] = $max;
+      $num=$mas[$i];
     }
-    else {
-      if($min>$mas[$i]) $min = $mas[$i];
-      $mas[$i] = $min;
-    }
+    if($mas[$i+1]=="") break;
+  }
+  $max=0;$min=$num;
+
+  for($i=0;$i<$count;$i++)
+  {
+    if($mas[$i]>$max) $max = $mas[$i];
+    if($min>$mas[$i]) $min = $mas[$i];
   }
   echo "<br>Измененный массив: ";
-  for($j=0;$j<10;$j++)
+  for($j=0;$j<$count;$j++)
   {
     if($j%2==0) $mas[$j] = $max;
+    else $mas[$j] = $min;
     echo "$mas[$j] | ";
   }
 ?>
